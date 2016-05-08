682a550575b738763397cfbe0ecd2e77cdf85e58
jraizes <jlraizes@gmail.com>
2016-05-07 18:23:53 -0700

    Simon.c inline assembly optimized for speed. Simon.c split into Simon.c and Simon.h for better modularity.

--- Simon/Simon.c
+++ Simon/Simon.c
@@ -1,9 +1,12 @@
-# include	<stdint.h>
-# include	<stdio.h>
+/*
+ * Simon 128/256 Cipher in C and inline assembly
+ *
+ * This code is made public using the MIT License
+ *
+ * Author: Justin Raizes
+ */
 
-void R(uint64_t* k, uint64_t* x, uint64_t* y);
-
-int main(int argc, char* argv){}
+ # include	"Simon.h"
 
 /*
  * A basic Simon round.
@@ -23,23 +26,28 @@ void R(uint64_t* k, uint64_t* x, uint64_t* y){
 		which is not natively supported in c.
 	 */
 	asm(
-		/* (S1x & S8x) */
-		"mov	%1, %%rax \n\t"		// Load tmp into rax
-		"rol	$1, %%rax \n\t" 	// S1x on rax
-		"mov	%1, %%rbx \n\t" 	// Load tmp into rbx
-		"rol	$8, %%rbx \n\t"		// S8x on rbx
-		"and	%%rbx, %%rax \n\t"	//AND into rax
-		/* (S1x & S8x) ^ S2x */
-		"mov	%1, %%rbx \n\t"		// Load tmp into rbx
-		"rol	$2, %%rbx \n\t" 	// S2x on rbx
-		"xor	%%rbx, %%rax \n\t"	// XOR into rax
-		/* XOR with current value of x */
-		"mov	%2, %%rbx \n\t"		// Load x into rbx
-		"xor	%%rbx, %%rax \n\t"	// XOR into rax
-		"mov	%%rax, %0"			// Save into x
-		: "=r" (*x)
-		: "r" (tmp), "r" (*x)
-		: "rax", "rbx"
+		/* Load the original value of x into rax, rbx, rcx */
+		"mov	%1, %%rax \n\t"
+		"mov	%%rax, %%rbx \n\t"
+		"mov	%%rax, %%rcx \n\t"
+
+		/* Compute (S1x & S8x) */
+		"rol	$1, %%rax \n\t"
+		"rol	$8, %%rbx \n\t"
+		"and	%%rbx, %%rax \n\t"
+
+		/* Compute (S1x & S8x) ^ S2x */
+		"rol	$2, %%rcx \n\t"
+		"XOR	%%rcx, %%rax \n\t"
+
+		/* XOR with current value of x and save */
+		"mov	%2, %%rbx \n\t"
+		"xor	%%rbx, %%rax \n\t"
+		"mov	%%rax, %0"
+
+		:"=r" (*x)
+		:"r" (tmp), "r" (*x)
+		: "rax", "rbx", "rcx"
 		);
 
 	*y = tmp;
---
+++ Simon/Simon.h
@@ -0,0 +1,3 @@
+# include	<stdint.h>
+
+void R(uint64_t* k, uint64_t* x, uint64_t* y);
